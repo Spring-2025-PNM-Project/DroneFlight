198d5e37f919416107ef445d83283fd50dfcc93b
Kyle Chu <kc5548092@gmail.com>
2025-03-23 20:58:35 -0700

    Correct connection string with com4

--- droneflight.py
+++ droneflight.py
@@ -1,6 +1,5 @@
 from dronekit import connect
-import socket
-import exceptions
-
-vehicle = connect('', wait_ready=True)
+# Connect to UDP endpoint.
+vehicle = connect('COM4', baud=57600, wait_ready=True)
+# Use returned Vehicle object to query device state - e.g. to get the mode:
 print("Mode: " + vehicle.mode.name)
